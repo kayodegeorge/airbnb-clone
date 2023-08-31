1b78a8c6c8cfaf52c26822e3c7ef5448c98d0af5
Kayode George <68115973+kayodegeorge@users.noreply.github.com>
2023-08-31 16:43:32 +0100

    pushed!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['avaters.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+}
 
 module.exports = nextConfig
